2c7011aeb24602f0b8e74a785b2f100d5ce6fceb
Florentino Romero Haro <fromeroharo@gmail.com>
2014-05-29 16:38:58 +0200

    firstCommit
    firtsCommit

---
+++ src/main/java/com/csd/Hamcrest/StringListInAnyOrder.java
@@ -0,0 +1,51 @@
+package com.csd.Hamcrest;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class StringListInAnyOrder extends TypeSafeMatcher<List<String>> {
+
+	List<String> lista2;
+
+	public StringListInAnyOrder(List<String> expected) {
+		this.lista2 = expected;
+	}
+
+	public void describeTo(Description description) {
+		description.appendText("list ignoring order ");
+		description.appendValue(lista2);
+	}
+
+	@Override
+	protected boolean matchesSafely(List<String> lista1) {
+		if (lista1.size()>0) {
+			for (String element : lista1) {
+					if (!listContainsElement(element)){
+						return false;
+					}
+			}
+			return true;
+		}
+		return areEmptyLists(lista1);
+	}
+
+	private boolean listContainsElement(String element) {
+		return lista2.contains(element);
+	}
+
+	private boolean areEmptyLists(List<String> lista1) {
+		return lista1.isEmpty() && lista2.isEmpty();
+	}
+
+	@Factory
+	public static Matcher<List<String>> stringListInAnyOrder(
+			List<String> expected) {
+		return new StringListInAnyOrder(expected);
+	}
+}
---
+++ src/test/java/com/csd/Hamcrest/StringListInAnyOrderTest.java
@@ -0,0 +1,37 @@
+package com.csd.Hamcrest;
+
+import static com.csd.Hamcrest.StringListInAnyOrder.stringListInAnyOrder;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class StringListInAnyOrderTest {
+	
+	@Test
+	public void sameListUnordered() throws Exception {
+		List<String> lista1 = Arrays.asList("elm2","elm1");
+		List<String> lista2 = Arrays.asList("elm1","elm2");
+		assertThat(lista1, stringListInAnyOrder(lista2));
+	}
+	
+	@Test
+	public void checkingTwoEmptyList() throws Exception {
+		List<String> lista1 = new ArrayList<String>();
+		List<String> lista2 = new ArrayList<String>();
+		assertThat(lista1, stringListInAnyOrder(lista2));
+	}
+	
+	@Test
+	public void checkingTwoOneElementListEquals() throws Exception {
+		List<String> lista1 = Arrays.asList("elm1");
+		List<String> lista2 = Arrays.asList("elm1");
+		assertThat(lista1, stringListInAnyOrder(lista2));
+	}
+	
+	
+}
